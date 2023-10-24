8fb38f7739f20b433560ec12e38af2124ae05d68
Venyus <venyus.boldrini@ewally.com.br>
2023-10-24 19:26:29 -0300

    feat: add status component

--- src/app/status/status.component.ts
+++ src/app/status/status.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-status',
   templateUrl: './status.component.html',
-  styleUrls: ['./status.component.css']
+  styleUrls: ['./status.component.css'],
 })
-export class StatusComponent {
-
-}
+export class StatusComponent {}
